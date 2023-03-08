5882c63efba2464df84dbca0dceeeb5c2a2edc6f
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2023-03-09 00:16:52 +0200

    Add AAVE_WHALE and AAVE_ADDR

--- test/config.js
+++ test/config.js
@@ -1,13 +1,16 @@
 const USDC_ADDR = process.env.USDC_ADDR;
 const DAI_ADDR = process.env.DAI_ADDR;
+const AAVE_ADDR = process.env.AAVE_ADDR;
 
 const AAVE_POOL_ADDRESS_PROVIDER = process.env.AAVE_POOL_ADDRESS_PROVIDER;
-
 const USDC_DAI_WHALE = process.env.USDC_DAI_WHALE;
+const AAVE_WHALE = process.env.AAVE_WHALE;
 
 module.exports = {
     USDC_ADDR,
     DAI_ADDR,
+    AAVE_ADDR,
     USDC_DAI_WHALE,
+    AAVE_WHALE,
     AAVE_POOL_ADDRESS_PROVIDER
 }
