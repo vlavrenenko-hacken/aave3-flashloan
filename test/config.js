ec8f546b44e3ff623b81732363075aca30c10b33
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2023-03-08 18:38:53 +0200

    Add the address of the DAI token

--- test/config.js
+++ test/config.js
@@ -1,11 +1,13 @@
 const USDC_ADDR = process.env.USDC_ADDR;
+const DAI_ADDR = process.env.DAI_ADDR;
 
 const AAVE_POOL_ADDRESS_PROVIDER = process.env.AAVE_POOL_ADDRESS_PROVIDER;
 
-const USDC_WHALE = process.env.USDC_WHALE;
+const USDC_DAI_WHALE = process.env.USDC_DAI_WHALE;
 
 module.exports = {
     USDC_ADDR,
+    DAI_ADDR,
     USDC_WHALE,
     AAVE_POOL_ADDRESS_PROVIDER
 }
