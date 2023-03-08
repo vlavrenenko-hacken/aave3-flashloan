35a44f414c79628f98aa39e127eea48c6ca6f2e7
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2023-03-09 00:03:40 +0200

    Add FlashLoan for aave flashLoan

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -2,59 +2,51 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@aave/core-v3/contracts/interfaces/IPool.sol";
 import "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
-import "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import "@aave/core-v3/contracts/flashloan/base/FlashLoanReceiverBase.sol";
 import "hardhat/console.sol";
 
-contract AaveFlashloan is FlashLoanSimpleReceiverBase {
-    using SafeMath for uint256;
+contract AaveFlashloan is FlashLoanReceiverBase {
     using SafeERC20 for IERC20;
+    
+    constructor(IPoolAddressesProvider provider) FlashLoanReceiverBase(provider) {}
 
-    constructor(IPoolAddressesProvider provider)
-        FlashLoanSimpleReceiverBase(provider)
-    {}
+    function aaveFlashloan(address[] calldata assets, uint256[] calldata amounts, uint256[] calldata interestRateModes) external {
+        for (uint i; i < assets.length;) {
+            uint fee = (amounts[i] * 5) / 10000;
+            
+            IERC20(assets[i]).transferFrom(msg.sender, address(this), fee); // User should have approved the fee amount before calling aaveFlashloan() function
+                unchecked{ ++i; }
+            }
 
-    function aaveFlashloanSimple(address loanToken, uint256 loanAmount) external {
-        uint fee = ((loanAmount*5)/10000);
-        IERC20(loanToken).transferFrom(msg.sender, address(this), fee); // User should have approve the fee amount before calling aaveFlashloan() function
-        IPool(address(POOL)).flashLoanSimple(
-            address(this),
-            loanToken,
-            loanAmount,
-            "0x",
-            0
-        );
+            IPool(address(POOL)).flashLoan(address(this), assets, amounts, interestRateModes, address(this), "0x", 0);
     }
-
-  /**
-   * @notice Executes an operation after receiving the flash-borrowed asset
+  
+   /**
+   * @notice Executes an operation after receiving the flash-borrowed assets
    * @dev Ensure that the contract can return the debt + premium, e.g., has
    *      enough funds to repay and has approved the Pool to pull the total amount
-   * @param asset The address of the flash-borrowed asset
-   * @param amount The amount of the flash-borrowed asset
-   * @param premium The fee of the flash-borrowed asset
-   * @param initiator The address of the flashloan initiator
-   * @param params The byte-encoded params passed when initiating the flashloan
-   * @return True if the execution of the operation succeeds, false otherwise
+   * @param assets The addresses of the flash-borrowed assets
+   * @param amounts The amounts of the flash-borrowed assets
+   * @param premiums The fee of each flash-borrowed asset
    */
     function executeOperation(
-        address asset,
-        uint256 amount,
-        uint256 premium, // fee
-        address initiator, // initiator
-        bytes memory params
-    ) public override returns (bool) {
-        require(
-            amount <= IERC20(asset).balanceOf(address(this)),
-            "Invalid balance for the contract"
-        );
-        console.log("borrowed amount:", amount);
-        console.log("flashloan fee: ", premium);
-
-        IERC20(asset).approve(address(POOL), amount.add(premium));
-
+        address[] calldata assets,
+        uint256[] calldata amounts,
+        uint256[] calldata premiums,
+        address,
+        bytes calldata
+    ) external returns (bool) {
+        require(assets.length == amounts.length && amounts.length == premiums.length, "Incorrect params");
+        for (uint i; i < premiums.length;) {
+            console.log("--------------");
+            console.log("Borrowed asset: ", assets[i]);
+            console.log("borrowed amount:", amounts[i]);
+            console.log("flashloan fee: ", premiums[i]);
+            IERC20(assets[i]).approve(address(POOL), amounts[i] + premiums[i]);
+            unchecked{++i;}
+        }
         return true;
     }
 }
