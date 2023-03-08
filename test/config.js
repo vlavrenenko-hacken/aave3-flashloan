d14f688011131df0e3a8d1494472ef0032226be1
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2023-03-08 17:01:51 +0200

    Add js config

---
+++ test/config.js
@@ -0,0 +1,11 @@
+const USDC_ADDR = process.env.USDC_ADDR;
+
+const AAVE_POOL_ADDRESS_PROVIDER = process.env.AAVE_POOL_ADDRESS_PROVIDER;
+
+const USDC_WHALE = process.env.USDC_WHALE;
+
+module.exports = {
+    USDC_ADDR,
+    USDC_WHALE,
+    AAVE_POOL_ADDRESS_PROVIDER
+}
